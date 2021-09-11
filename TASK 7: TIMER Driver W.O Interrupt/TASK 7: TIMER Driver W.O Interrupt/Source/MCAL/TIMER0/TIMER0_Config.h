37e9f8c644b7ff7f534ba260ebb75efa37847ca7
Joe Khaled <youssef.khaled.farag@gmail.com>
2021-09-11 06:07:36 +0200

    V0.1

---
+++ TASK 7: TIMER Driver W.O Interrupt/TASK 7: TIMER Driver W.O Interrupt/Source/MCAL/TIMER0/TIMER0_Config.h
@@ -0,0 +1,40 @@
+#ifndef TIMER0_Config_h
+#define TIMER0_Config_h
+
+#define SIGNAL_MODE                    Normal_Mode
+#define COMPARE_OUTPUT_MODE            OC0_DISCONNECTED
+#define CLOCK_Prescaler                Clk_1024
+
+/*
+ 
+ SIGNAL_MODE CHOICES:-
+ -----------------------
+ Normal_Mode
+ PWM_PhaseCorrect
+ CTC
+ Fast_PWM
+ 
+ COMPARE_OUTPUT_MODE CHOICES:-
+ ------------------------------
+ 
+ OC0_DISCONNECTED
+ OC0_TOGGLE_ON_COMPARE_MATCH
+ OC0_CLEAR_ON_COMPARE_MATCH
+ OC0_SET_ON_COMPARE_MATCH
+ 
+ CLOCK_TYPES_Prescalers CHOICES :-
+ ----------------------------------
+ No_Clock_Stop
+ ClkIo
+ Clk_8
+ Clk_64
+ Clk_256
+ Clk_1024
+ Ext_clk_FallingEdge
+ Ext_clk_RisingEdge
+ 
+ */
+
+
+
+#endif
